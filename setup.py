f163a700b2982933b99ab6e88e1d97eac8f8db1d
Srdjan Grubor <sgnn7@sgnn7.org>
2017-05-04 21:02:18 -0500

    Turned the toplevel dir into a module
    This should encompass all the smaller modules when running things but
    still keep them separate.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sgfc',
+    zip_safe = True,
+    version='0.1',
+    url='',
+    description='',
+    license='',
+    author='Srdjan Grubor',
+    author_email='sgnn7@sgnn7.org',
+    packages=find_packages(),
+    install_requires = [
+        'sgfc_communication',
+        'sgfc_control',
+    ],
+)
