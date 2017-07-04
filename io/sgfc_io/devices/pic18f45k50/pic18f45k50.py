ef84e8abf1d148a0399828dbd266aea9182a6b6d
Srdjan Grubor <sgnn7@sgnn7.org>
2017-07-04 13:13:35 -0500

    Converted io code to use PCA9685 as I2C
    We now use USB -> PIC18 -> I2C -> PCA9685 -> PWM. PIC18 was ridiculously
    hard to configure for muliple pwms so I opted for the easy approach with
    a board that has very simple APIs - we still need our PIC to communicate
    over USB to it though.

--- io/sgfc_io/devices/pic18f45k50/pic18f45k50.py
+++ io/sgfc_io/devices/pic18f45k50/pic18f45k50.py
@@ -7,44 +7,100 @@ from .hk_usb_io import close as usb_close
 
 from ..api_interface import IoDeviceApi
 
+from .pca9685 import PCA9685, PCA9685_ADDRESS
+
+
+# Shim for Adafruit / RPi I2C compat
+class I2cComm(object):
+    def __init__(self, usb, debug=False):
+        self._usb = usb
+        self._debug = debug
+
+        i2c_init(self._usb)
+
+    def get_i2c_device(self, device_address, *args, **kwargs):
+        self._device_addr = device_address
+        return self
+
+    def write8(self, addr, value):
+        # Clip value to 1 byte
+        value = value & 0xFF
+
+        if self._debug:
+            print("Writing to %s[%s]: %s" % (hex(self._device_addr),
+                                             hex(addr),
+                                             hex(value)))
+
+        i2c_start(self._usb, I2C_START_CMD)
+        i2c_write(self._usb, (self._device_addr << 1 | I2C_WRITE_CMD))
+        while(i2c_slave_ack(self._usb)):
+            time.sleep(0.1)
+
+        # Select the register to write to
+        i2c_write(self._usb, addr)
+        while (i2c_slave_ack(self._usb)):
+            time.sleep(0.1)
+        # Write the value to the previously selected reg
+        i2c_write(self._usb, value)
+        while (i2c_slave_ack(self._usb)):
+            time.sleep(0.1)
+        i2c_stop(self._usb)
+
+    def readU8(self, addr):
+        i2c_start(self._usb, I2C_START_CMD)
+        i2c_write(self._usb, (self._device_addr << 1 | I2C_WRITE_CMD))
+        while (i2c_slave_ack(self._usb)):    # wait for slave ack
+            time.sleep(0.1)
+        # select the register address to read
+        i2c_write(self._usb, addr)
+        while (i2c_slave_ack(self._usb)):    # wait for slave ack
+            time.sleep(0.1)
+        i2c_start(self._usb, I2C_REP_START_CMD)
+        i2c_write(self._usb, (self._device_addr << 1 | I2C_READ_CMD))
+        # read the value from the register
+        while (i2c_slave_ack(self._usb)):    # wait for slave ack
+            time.sleep(0.1)
+        ret_val = i2c_read(self._usb)
+        # ack the byte
+        i2c_master_ack(self._usb, I2C_DATA_NOACK)
+        i2c_stop(self._usb)
+        return ret_val
+
 
 class Pic18F45K50IoDevice(IoDeviceApi):
-    def __init__(self, debug=False):
+    PWM_FREQUENCY = 500
+
+    def __init__(self, flags=0x0, debug=False):
         self._debug = debug
         self._usb = init()
 
-        self._pwm = Bunch(PIC_REGISTER)
-        self._bit = Bunch(PIC_BITS)
-
-        # Set all registers RD* and RB* to digital
-        sfr_set_regbit(self._usb, self._pwm.ANSELD, 0, 0)
-
-        # Set all RD* and all RB* to output
-        sfr_set_regbit(self._usb, self._pwm.TRISD,  0, dir_input)
-
-        # PWM timer selection C/C == TMR1, PWM == TMR2
-        sfr_set_reg(self._usb, self._pwm.CCPTMRS, 0x00)
-
-        # CCP PWM mode
-        sfr_set_reg(self._usb, self._pwm.CCP1CON, 0b00001100)
-
-        sfr_set_reg(self._usb, self._pwm.PR2, 0xBA)
-        sfr_set_reg(self._usb, self._pwm.CCPR1L, 0x00)
-        sfr_set_reg(self._usb, self._pwm.T2CON, 0b01111110)
-
-        # Define output
-        sfr_set_regbit(self._usb, self._pwm.PSTR1CON, 1, 0b00001111)
-        sfr_set_regbit(self._usb, self._pwm.TRISD,  0b00000101, dir_output)
-
-        sfr_set_reg(self._usb, self._pwm.CCPR1L, 0x22)
+        self._using_i2c = flags and ((flags | IoDeviceApi.I2C) == IoDeviceApi.I2C)
+        if self._using_i2c:
+            print("Using I2C mode")
+            self._i2c = I2cComm(self._usb)
+            self._pca9685 = PCA9685(i2c=self._i2c)
+            self._pca9685.set_pwm_freq(Pic18F45K50IoDevice.PWM_FREQUENCY)
 
     def close(self):
         usb_close(self._usb)
 
-    def set_front_left_pwm(self, ratio):
+    def _set_channel_pwm(self, channel, ratio):
         print("Ratio:", ratio)
+        off_at=int(4095 * ratio)
+        self._pca9685.set_pwm(channel, 0x00, off_at)
 
-        value = int(ratio * 0xBA)
-        print("Register:", "0x" + "{:02x}".format(value).upper())
+    def set_front_left_pwm(self, ratio):
+        if self._using_i2c:
+            self._set_channel_pwm(1, ratio)
 
-        sfr_set_reg(self._usb, self._pwm.CCPR1L, value)
+    def set_front_right_pwm(self, ratio):
+        if self._using_i2c:
+            self._set_channel_pwm(2, ratio)
+
+    def set_back_left_pwm(self, ratio):
+        if self._using_i2c:
+            self._set_channel_pwm(0, ratio)
+
+    def set_back_right_pwm(self, ratio):
+        if self._using_i2c:
+            self._set_channel_pwm(3, ratio)
