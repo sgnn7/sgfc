144a09549c5d4f0d9923a83cf04c53edfa888e02
Srdjan Grubor <sgnn7@sgnn7.org>
2017-07-22 20:41:25 -0400

    Added a test comm recv script to comm module
    This makes it easier to test if the code is actually working.

---
+++ communication/comm_recv.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2
+
+import time
+
+import sgfc_communication
+
+from sgfc_communication.protobufs import sgfc_pb2 as fc_proto
+
+
+def comm_callback(data):
+    message = fc_proto.FlightMessage()
+    message.ParseFromString(data)
+    print(message)
+
+def comm_error_callback(error):
+    print(error)
+
+def open_comms():
+    comm_device = None
+    try:
+        comm_device = sgfc_communication.get_device('zigbee_xbee',
+                                                    '\x00\x02',
+                                                    comm_callback,
+                                                    comm_error_callback,
+                                                    device='/dev/ttyUSB0')
+
+    except KeyboardInterrupt as kbi:
+        if comm_device:
+            comm_device.close()
+
+if __name__ == '__main__':
+    open_comms()
